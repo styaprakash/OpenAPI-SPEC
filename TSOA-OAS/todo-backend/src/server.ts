0be0503a1819a14facae4d5a2b7b5ab809d9e5f6
satyaprakashmishra <satyaprakashmishra23.12@gmail.com>
2024-12-29 12:28:04 +0530

    Created the TS-client for this swagger.

--- TSOA-OAS/todo-backend/src/server.ts
+++ TSOA-OAS/todo-backend/src/server.ts
@@ -1,8 +1,11 @@
 // src/server.ts
 import { app } from "./app";
+import { DefaultService } from "../node-client";
 
 const port = process.env.PORT || 3000;
 
 app.listen(port, () =>
   console.log(`Example app listening at http://localhost:${port}`)
 );
+
+DefaultService.getTodo("asd")
